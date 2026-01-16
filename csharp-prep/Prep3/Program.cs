6f1b8def1363421b70da636449b1536f2eb22186
rikkyd31 <168210352+rikkyd31@users.noreply.github.com>
2026-01-15 19:29:05 -0700

    Completed Prep 03

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playGame = "yes";
+
+        while (playGame.ToLower() == "yes" || playGame.ToLower() == "y")
+        {
+            
+            Console.Write("Would you like to play the guessing game? ");
+            playGame = Console.ReadLine();
+
+            if (playGame.ToLower() != "yes" && playGame.ToLower() != "y")
+            {
+                break;
+            }           
+
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 51);
+            Console.WriteLine("Generating a random number between 1 and 50!");
+
+            int guessNum = 0;
+            int guessCount = 0;
+            do {
+
+                Console.Write("What is your guess? ");
+                string guessString = Console.ReadLine();
+                guessNum = int.Parse(guessString);
+
+                if (guessNum > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                    guessCount ++;
+
+                }
+                else if (guessNum < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                    guessCount ++;
+                }
+                else if (guessNum == magicNum)
+                {
+                    Console.WriteLine("You Guessed It!");
+                    guessCount ++;
+                    Console.WriteLine($"You guessed {guessCount} times!");
+                }
+                else
+                {
+                    Console.WriteLine("Not a Number");
+                }
+
+            } while (guessNum != magicNum);
+        }
     }
 }
