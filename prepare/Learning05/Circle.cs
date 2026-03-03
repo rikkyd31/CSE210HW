de07437cb6ad8e37222687361bda933a80b19c25
rikkyd31 <168210352+rikkyd31@users.noreply.github.com>
2026-03-03 08:58:39 -0700

    March 2, 2026 Completed Preparation 05

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,16 @@
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _color = color;
+        _radius = radius;   
+    }
+
+    public override double GetArea()
+    {
+        double area = (_radius * _radius) * Math.PI;
+        return area;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Drawing;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapesList = new List<Shape>();
+        string color = "Red";
+        double side = 3.0;
+        double width = 2.0;
+        double height = 4.0;
+        double radius = 2.0;
+        double area;
+
+        shapesList.Add(new Square(color, side));
+        shapesList.Add(new Rectangle(color, width, height));
+        shapesList.Add(new Circle(color, radius));
+
+        foreach (Shape shape in shapesList)
+        {
+            color = shape.GetColor();
+            area = shape.GetArea();
+            Console.WriteLine(color);
+            Console.WriteLine(area);
+        }
     }
 }
